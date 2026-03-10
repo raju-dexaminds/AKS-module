4f3b98b593f33c794f968af9aa00878dacecb622
Raju <raju.b@dexaminds.com>
2026-03-10 16:23:28 +0530

    commit the changes

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,9 @@
 output "resource_group_name" {
   value = module.resource_group.resource_group_name
 }
-
 output "aks_cluster_name" {
   value = module.aks.aks_cluster_name
 }
-
 output "acr_login_server" {
   value = module.acr.acr_login_server
 }
